7fefa0ab7e99fe6be8d6b99f4b3ccd896a85febe
alivam <www.alinajafi.ir@gmail.com>
2016-06-19 06:50:40 -0700

    Update ver.lua

--- plugins/ver.lua
+++ plugins/ver.lua
@@ -1,19 +1,10 @@
 do
 
 function run(msg, matches)
-  return [[ B L A C K +
------------------------------------
+  return [[
 A new bot for manage your Supergroups.
 -----------------------------------
-@Black_CH #Channel
------------------------------------
-@MehdiHS #Developer
------------------------------------
-@YflQw #Manager
------------------------------------
-@Mr_Surena #Manager
------------------------------------
-Bot number : +19092545429
+@v4mpire
 -----------------------------------
 Bot version : 6.7 ]]
 end
